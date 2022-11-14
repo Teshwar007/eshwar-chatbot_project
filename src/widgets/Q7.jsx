9983e16107fbf560f455a7d67597bad8c781edd8
Eshwar Prasad <eshwarprasad786@gmail.com>
2022-11-14 15:55:53 -0600

    added music

--- src/widgets/Q7.jsx
+++ src/widgets/Q7.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import { useState } from "react";
 
 //defination  of Question1 widget
 //all the mathod define in actionprovider and all states of widget are passed in props
@@ -11,7 +9,6 @@ const Q7 = (props) => {
     <>
       <ul>
         <li>
-            <Button style={{ background: 'yellow' }}> <img src="https://cdn-icons-png.flaticon.com/128/0/375.png"/> Sounds like you are a {localStorage.getItem("")}  </Button>
           <iframe  src= {'https://open.spotify.com/embed/track/' + localStorage.getItem("trackID") + '?utm_source=generator'}  width="100%" height="252" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture" loading="lazy"></iframe>
         </li>
       </ul>
